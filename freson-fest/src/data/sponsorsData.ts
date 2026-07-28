0199975b83e2daeccdbdf2c879ec847d145e7745
Lulencina <lulencina@hotmail.com>
2026-07-27 22:33:53 -0300

    Implement Sponsors-section

---
+++ freson-fest/src/data/sponsorsData.ts
@@ -0,0 +1,52 @@
+/* eslint-disable prettier/prettier */
+export interface Sponsor {
+    image?: string;
+    description: string;
+}
+
+export interface SponsorCategory {
+    categoryName: string;
+    sponsors: Sponsor[];
+}
+
+export const sponsorCategories: SponsorCategory[] = [
+  {
+    categoryName: "Patrocinador Nombre",
+    sponsors: [
+      {
+        image: "",
+        description: "Breve descripción del patrocinador 1",
+      },
+      {
+        image: "",
+        description: "Breve descripción con microcopy del patrocinador",
+      },
+    ],
+  },
+  {
+    categoryName: "Patrocinador Nombre 2",
+    sponsors: [
+      {
+        image: "",
+        description: "Breve descripción del patrocinador 2",
+      },
+      {
+        image: "",
+        description: "Breve descripción con microcopy del patrocinador",
+      },
+    ],
+  },
+  {
+    categoryName: "Patrocinador Nombre 3",
+    sponsors: [
+      {
+        image: "",
+        description: "Breve descripción del patrocinador 3",
+      },
+      {
+        image: "",
+        description: "Breve descripción con microcopy del patrocinador",
+      },
+    ],
+  },
+];
