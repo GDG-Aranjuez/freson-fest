41bae4b51d008a6a4598f454b37841688c39881d
Lulencina <lulencina@hotmail.com>
2026-08-29 18:25:50 -0300

    feat: add responsive ripple background effect to hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 const site = "https://gdg-aranjuez.github.io";
 const base = "/freson-fest/";
 
@@ -14,9 +16,7 @@ export default defineConfig({
   site,
   base,
   output: "static",
-  integrations: [
-    sitemap({
-      filter: (page) => !page.endsWith("/404/") && !page.endsWith("/404"),
-    }),
-  ],
+  integrations: [sitemap({
+    filter: (page) => !page.endsWith("/404/") && !page.endsWith("/404"),
+  }), react()],
 });
